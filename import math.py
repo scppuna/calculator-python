86c320aa4229a539db7624048fcb867e5f737d25
Mari Magalhães <104236643+scppuna@users.noreply.github.com>
2024-03-26 20:40:27 -0300

    including-program

---
+++ import math.py
@@ -0,0 +1,18 @@
+import math
+
+entrada1 = input("Informe o primeiro numero: ")
+entrada2 = input("Informe o segundo numero: ")
+ 
+x = float(entrada1)
+y = float(entrada2)
+ 
+soma = x + y
+subtracao = x - y
+multiplicacao = x * y
+divisao = x / y
+ 
+print("Resultados:\n"
+      "\nSoma:", soma,
+      "\nSubtração:", subtracao,
+      "\nMultiplicação:", multiplicacao,
+      "\nDivisão:", divisao)
